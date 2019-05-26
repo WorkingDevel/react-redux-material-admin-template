0f2c6ef98d91bf377b333a6c19be4497a2ad1197
Christoph Graupner <ch.graupner@workingdeveloper.net>
2019-05-26 08:58:52 +0200

    added material-ui base

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button, CssBaseline, Paper} from '@material-ui/core';
+import {ThemeProvider} from '@material-ui/styles';
+import theme from "./theme";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+
+      <div className="App">
+        <header className="App-header">
+          <Paper>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <Button variant="contained" color="primary">
+            Hello World
+          </Button>
+          </Paper>
+        </header>
+      </div>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
